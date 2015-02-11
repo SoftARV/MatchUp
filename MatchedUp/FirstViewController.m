2b318517087a6593715517c9166ace581073ea99
Miguel Rincon <miguelrincon@Miguels-MacBook-Pro.local>
2015-02-11 11:19:37 -0430

    Initial Commit

---
+++ MatchedUp/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  MatchedUp
+//
+//  Created by Miguel Rincon on 2/11/15.
+//  Copyright (c) 2015 Miguel Rincon. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ MatchedUp/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  MatchedUp
+//
+//  Created by Miguel Rincon on 2/11/15.
+//  Copyright (c) 2015 Miguel Rincon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
